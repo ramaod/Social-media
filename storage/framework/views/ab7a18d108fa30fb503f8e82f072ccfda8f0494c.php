1275ec8944a197e13d734dc6767f604ade5894a4
ramaod <107109900+ramaod@users.noreply.github.com>
2022-11-24 09:41:37 +0300

    Add files via upload

---
+++ storage/framework/views/ab7a18d108fa30fb503f8e82f072ccfda8f0494c.php
@@ -0,0 +1,26 @@
+
+<?php $__env->startSection('content'); ?>
+<div class="container" style="padding-top: 2%">
+    <div class="card">
+
+        <div class="card-body">
+
+            <p class="card-text"> Card footer</p>
+        </div>
+    </div>
+</div>
+<div class="container" style="padding-top: 2%"> 
+        <div class="form-group">
+            <label for="exampleFormControlInput1">Text</label>
+            <input type="text" name="text" class="form-control" placeholder="post text">
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlInput1">photo</label>
+            <input type="file" name="img" class="form-control" placeholder="post img">
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-info">Save</button></div>
+    </form>
+</div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('post.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\laravel project\socialmedia\resources\views/post/create.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/c69a65f6499f9339a0638aa1b0d57a83e8db9ad3.php
@@ -0,0 +1,72 @@
+
+
+<?php $__env->startSection('content'); ?>
+<div class="jumbotron container">
+
+    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+
+  </div>
+
+  <div class="container">
+
+    <table class="table">
+
+        <thead class="thead-dark">
+
+          <tr>
+
+
+            <th scope="col">text</th>
+
+            <th scope="col">img</th>
+
+
+          </tr>
+
+        </thead>
+
+        <tbody>
+          <?php
+              $i = 0;
+          ?>
+         <?php $__currentLoopData = $post; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+
+            <tr>
+
+                <th scope="row"><?php echo e(++$i); ?></th>
+
+                <td><?php echo e($item->text); ?></td>
+
+                <td><?php echo e($item->img); ?></td>
+
+                <td>
+
+                    <a href="<?php echo e(route('post.edit',$item->id)); ?>"></a>
+
+                    <a href="<?php echo e(route('post.show',$item->id)); ?>"></a>
+
+                    <form action="<?php echo e(route('post.destroy',$item->id)); ?>">
+
+                    <?php echo csrf_field(); ?>
+
+                    <?php echo method_field('DELETE'); ?>
+
+                    <BUtton type="submit" class="btn btn-danger">Delete</BUtton>
+
+                    </form>
+
+                </td>
+
+              </tr>
+
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+         
+        </tbody>
+      </table>
+      <?php echo $post->links(); ?>
+
+  </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('post.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\laravel project\socialmedia\resources\views/post/index.blade.php ENDPATH**/ ?>
